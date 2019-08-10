83d653337f3199c467df979c79fea62cf45279ee
Philip Van Kampen <plvankampen@gmail.com>
2019-08-10 17:49:53 -0400

    sat

---
+++ algorithms/assignments/Percolation/Percolation_Simulation/Percolation_Simulation.pro
@@ -0,0 +1,11 @@
+QT += core
+QT -= gui
+
+TARGET = Percolation_Simulation
+CONFIG += console
+CONFIG -= app_bundle
+
+TEMPLATE = app
+
+SOURCES += main.cpp
+
---
+++ algorithms/assignments/Percolation/Percolation_Simulation/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include<iostream>
+
+void monte_carlo_loop(int x)
+{
+    int loop_num = 1;
+    while(x)
+    {
+        std::cout << "Loop : " << loop_num++ << std::endl;
+        --x;
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QCoreApplication a(argc, argv);
+
+    // monte_carlo_loop(std::stoi(argv[1]));
+    monte_carlo_loop(123);
+
+
+    return a.exec();
+}
+
